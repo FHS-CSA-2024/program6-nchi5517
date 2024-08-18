0c1e640fecd8d57b2007ff8a462a46a1b082500c
nchi5517 <neelagreev.ch@gmail.com>
2024-08-18 13:59:22 -0700

    Update Program6.java
    finished program 6

--- Program6.java
+++ Program6.java
@@ -1,7 +1,36 @@
 //import stuff here!
+import java.math.RoundingMode;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 //Your code here
+public class Program6{
+    private static final DecimalFormat df = new DecimalFormat("0.000");
+    public static void main (String[] args){
+        double Radius = 0.0;
+        double area = 0.0;
+        double diameter = 0.0;
+        double circumference = 0.0;
+        final double pi = 3.14159;
+        Scanner myObj = new Scanner(System.in);
 
+        System.out.println("Enter Radius: ");
+        Radius = myObj.nextDouble();
+        
+        
+        area = pi*(Radius*Radius);
+         diameter = 2*Radius;
+         circumference = 2*pi*Radius; 
+        
+         
+        System.out.println("The Radius of a circle is "+Radius);
+        System.out.println("The Diameter of a circle = "+ diameter);
+        df.setRoundingMode(RoundingMode.UP);
+        System.out.println("The Area of a circle = "+ df.format(area));
+        df.setRoundingMode(RoundingMode.DOWN);
+        System.out.println("The Circumference of the circle = "+ df.format(circumference));
+    }
+}
 //Paste console output below:
 /*
 
